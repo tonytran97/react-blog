4b964562b385fe55e96fe1b314f2d429c1d9355f
tonytran97 <tonytran_97@live.com>
2023-04-06 16:21:34 -0700

    populated collections page w/ appropriate data and links

--- src/pages/Collection.js
+++ src/pages/Collection.js
@@ -1,6 +1,17 @@
+import { Link } from "react-router-dom";
+import sampleArticles from "../data/Article-Samples";
+
 const Collection = () => {
     return (
+        <>
         <h1>Article Collection List</h1>
+        {sampleArticles.map(article => (
+            <Link key={article.name} to={`/article/${article.name}`}>
+                <h3>{article.title}</h3>
+                <p>{article.content.substring(0, 150)}...</p>
+            </Link>     
+        ))}
+        </>
     );
 }
 
