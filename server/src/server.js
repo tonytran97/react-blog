2535a545ef690d1980bdadb947942e671c7642ef
tonytran97 <tonytran_97@live.com>
2023-04-08 20:08:54 -0700

    endpoint for upvote functionality, added nodemon

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,28 @@
 import express from 'express';
 
-const app = express();
+let articleInfo = [{
+    name: 'learning-react',
+    upvote: 0,
+}, {
+    name: 'learning-node', 
+    upvote: 0,
+}, {
+    name: 'learning-mongo',
+    upvote: 0,
+}]
 
-app.get('/hello', (req, res) => {
-    res.send('Hello World');
+const app = express();
+app.use(express.json());
+
+app.put('/api/article/:name/upvote', (req, res) => {
+    const { name } = req.params;
+    const article = articleInfo.find(a => a.name === name);
+    if (article){
+        article.upvote += 1;
+        res.send(`The ${name} article now has ${article.upvote} upvotes`);
+    } else {
+        res.send(`That article doesn't exist`);
+    }
 });
 
 app.listen(8000, () => {
