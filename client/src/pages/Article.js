1b6393460e0f7de6105a2307a5ca48521fbd7ad9
tonytran97 <tonytran_97@live.com>
2023-04-11 20:30:49 -0700

    loading upvotes to articles using axios

--- client/src/pages/Article.js
+++ client/src/pages/Article.js
@@ -1,9 +1,22 @@
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 import sampleArticles from '../data/Article-Samples';
 import NotFound from './404';
 
 const Article = () => {
+    const [articleInfo, setArticleInfo] = useState({ upvote: 0, comments: [] });
     const { articleID } = useParams();
+
+    useEffect(() => {
+        const loadArticleInfo = async () => {
+            const response = await axios.get(`/api/article/${articleID}`);
+            const newArticleInfo = response.data;
+            setArticleInfo(newArticleInfo);
+        }
+        loadArticleInfo();
+    }, []);
+
     const article = sampleArticles.find(article => article.name === articleID);
 
     if (!article) {
@@ -11,6 +24,7 @@ const Article = () => {
     }
 
     return <><h1>{article.title}</h1>
+    <p>This article has {articleInfo.upvote} upvote(s)</p>
     {article.content}
     </>
 };
