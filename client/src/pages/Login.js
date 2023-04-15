f15852fa67e771ba9e545cf1f7e0bdd4f47744c7
tonytran97 <tonytran_97@live.com>
2023-04-14 21:46:22 -0700

    signIn functionality to login btn

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,10 +1,23 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+
 
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
+
+    const navigate = useNavigate();
+    
+    const signIn = async () => {
+        try {
+            await signInWithEmailAndPassword(getAuth(), email, password);
+            navigate('/articles');
+        } catch (e) {
+            setError(e.message);
+        }
+    }
 
     return(
         <>
@@ -19,7 +32,7 @@ const Login = () => {
         placeholder='Password'
         value={password}
         onChange={e => setPassword(e.target.value)} />
-        <button>Login</button>
+        <button onClick={signIn}>Login</button>
         <Link to="/create-account">Don't have an account? Click here!</Link>
         </>
     );
