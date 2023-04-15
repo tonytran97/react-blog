20baef9a869a28147df6cfa3eb9ac2df31dbab18
tonytran97 <tonytran_97@live.com>
2023-04-14 21:38:39 -0700

    basic login page layout

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,6 +1,27 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
     return(
+        <>
         <h1>Login</h1>
+        {error && <p>{error}</p>}
+        <input
+         placeholder='Email address'
+         value={email}
+         onChange={e => setEmail(e.target.value)} />
+        <input 
+        type="password"
+        placeholder='Password'
+        value={password}
+        onChange={e => setPassword(e.target.value)} />
+        <button>Login</button>
+        <Link to="/create-account">Don't have an account? Click here!</Link>
+        </>
     );
 }
 
