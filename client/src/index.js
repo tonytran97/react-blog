f52e976c711939cdd64f75e7a6e3bfe3e1d83276
tonytran97 <tonytran_97@live.com>
2023-04-14 20:50:05 -0700

    adding firebase for authentication

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCAM2idZjHvxBwpHuBSmxL6fEV19QxzUrI",
+  authDomain: "react-blog-7662e.firebaseapp.com",
+  projectId: "react-blog-7662e",
+  storageBucket: "react-blog-7662e.appspot.com",
+  messagingSenderId: "962185046849",
+  appId: "1:962185046849:web:ad9250987eaed6000ecfc0",
+  measurementId: "G-GB7FD971N8"
+};
+
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
